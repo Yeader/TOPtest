e5a79e8c294288b31d61342bbfe9c22bf7730890
dass <1007090232@qq.com>
2018-06-28 17:32:29 +0800

    IOSscreen

---
+++ Assets/Script/ButtonsObj.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ButtonsObj
+{
+    private Transform _keypress;
+    public Transform Keypress
+    {
+        get { return _keypress; }
+        set { _keypress = value; }
+    }
+
+    private float _speed;
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+    private int _number;
+    public int Number
+    {
+        get { return _number; }
+        set {
+            _number = 0;
+            _number = value;
+        }
+    }
+
+    private Vector2 _downpos;
+    public Vector2 Downpos
+    {
+        get { return _downpos; }
+        set { _downpos = value; }
+    }
+
+    private Vector2 _uppos;
+    public Vector2 Uppos
+    {
+        get { return _uppos; }
+        set { _uppos = value; }
+    }
+
+    private Vector3 _movedir;
+    public Vector3 Movedir
+    {
+        get { return _movedir; }
+        set { _movedir = value; }
+    }
+
+    private bool _ismove;
+    public bool Ismove
+    {
+        get { return _ismove; }
+        set { _ismove = value; }
+    }
+
+    private Vector2 _firstyucunPos;
+    public Vector2 FirstyucunPos
+    {
+        get { return _firstyucunPos; }
+        set { _firstyucunPos = value; }
+    }
+
+    /// <summary>
+    /// 动画播放
+    /// </summary>
+    public void PlayerAni(string animation, Transform transone, Transform transtwo)
+    {
+        Keypress.GetComponent<Animator>().Play(animation);
+        //Keypress.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.farClipPlane));
+        Keypress.GetComponent<BoxCollider>().enabled = true;
+        transone.GetComponent<BoxCollider>().enabled = false;
+        transtwo.GetComponent<BoxCollider>().enabled = false;
+    }
+    public void PlayerAni(string animation)
+    {
+        Keypress.GetComponent<Animator>().Play(animation);
+    }
+
+    public void EnableTrue()
+    {
+        Keypress.GetComponent<BoxCollider>().enabled = true;
+    }
+
+    public void Move()
+    {
+        Keypress.Translate(-Movedir * Speed * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/Script/ClickTest.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickTest : MonoBehaviour
+{
+
+    Ray myray;
+    //RaycastHit myhit;
+    ButtonsObj yucun, meishi, haigang;
+    public Animator yucunani;
+    public Image showUI;
+    public Sprite yucunjietu, meishijietu, haigangjietu;
+    private Type type;
+    UDPClient myclient;
+    // Use this for initialization
+    void Start()
+    {
+        yucun = new ButtonsObj();
+        meishi = new ButtonsObj();
+        haigang = new ButtonsObj();
+        yucun.Keypress = transform.Find("Panel/yucun");
+
+        yucun.Ismove = true;
+
+        Screen.SetResolution(2048, 1536, false);
+        Debug.logger.logEnabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(myray))
+            {
+                yucun.Ismove = false;
+                yucun.Downpos = Camera.main.WorldToScreenPoint(Input.mousePosition);
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (yucun.Ismove == false)
+            {
+                yucun.Uppos = Camera.main.WorldToScreenPoint(Input.mousePosition);
+                yucun.Ismove = true;
+            }
+            else
+            {
+                yucun.Uppos = yucun.Downpos;
+            }
+
+            if ((yucun.Downpos.y - yucun.Uppos.y) < 0)
+            {
+                yucun.Ismove = true;
+                yucun.PlayerAni("anifly");
+                startnumber();
+            }
+            else
+            {
+
+            }
+        }
+        //Debug.Log(yucun.Keypress.localPosition.y);
+        if (yucun.Keypress.localPosition.y > 1200f)
+        {
+            switch (type)
+            {
+                case Type.yucun:
+                    Debug.Log("yucun");
+                    UDPClient.Instance.SocketSend("yucun");
+
+                    type = Type.initial;
+                    break;
+                case Type.meishi:
+                    Debug.Log("meishi");
+                    UDPClient.Instance.SocketSend("meishi");
+                    type = Type.initial;
+                    break;
+                case Type.haigang:
+                    Debug.Log("haigang");
+                    UDPClient.Instance.SocketSend("haigang");
+                    type = Type.initial;
+                    break;
+            }
+            yucunani.Play("keke");
+        }
+    }
+
+    public void PlayYucun()
+    {
+        type = Type.yucun;
+        showUI.sprite = yucunjietu;
+        if (yucun.Number == 0)
+        {
+            yucunani.Play("yucunmove");
+            startnumber();
+            yucun.Number = 1;
+        }
+        else
+        {
+            yucunani.Play("yucunback");
+            startnumber();
+        }
+
+    }
+
+    public void PlayMeishi()
+    {
+        type = Type.meishi;
+        showUI.sprite = meishijietu;
+        if (meishi.Number == 0)
+        {
+            yucunani.Play("meishimove");
+            startnumber();
+            meishi.Number = 1;
+        }
+        else
+        {
+            yucunani.Play("meishiback");
+            startnumber();
+        }
+
+    }
+    public void PlayHaigang()
+    {
+        type = Type.haigang;
+        showUI.sprite = haigangjietu;
+        if (haigang.Number == 0)
+        {
+            yucunani.Play("haigangmove");
+            startnumber();
+            haigang.Number = 1;
+        }
+        else
+        {
+            yucunani.Play("haigangback");
+            startnumber();
+        }
+
+    }
+    public void startnumber()
+    {
+        meishi.Number = 0;
+        haigang.Number = 0;
+        yucun.Number = 0;
+    }
+    public enum Type
+    {
+        initial,
+        yucun,
+        meishi,
+        haigang
+    }
+
+}
